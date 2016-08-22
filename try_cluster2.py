c0a7ecb469de6ee394241b8dad0002892afbceb7
EmilMachine <emil.lime@gmail.com>
2016-08-22 21:11:17 +0200

    with cluster try

---
+++ try_cluster2.py
@@ -0,0 +1,61 @@
+
+import pandas as pd
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.cluster import DBSCAN
+from sklearn import metrics
+import pylab
+
+filename_out = './data/train_short.csv'
+
+filename_in = './data/train.csv'
+my_data = pd.read_csv(filename_in,nrows=5000);
+my_data.to_csv(filename_out,index = False);
+
+my_data = pd.read_csv(filename_out);
+
+
+
+#my_data['Sequence'] = my_data['Sequence'].apply(str.split,args=',')
+
+x = map(lambda x: [int(i) for i in x], my_data['Sequence'].apply(str.split,args=',').values)
+seq_lenght = 15
+
+x = [i[:seq_lenght] for i in x if len(i)>seq_lenght]
+print len(x)
+
+model = TSNE(n_components=2, random_state=0)
+
+X = np.array(model.fit_transform(np.array(x)))
+
+
+
+## ===============
+## 
+## ===============
+###############################################################################
+# Compute DBSCAN
+db = DBSCAN(eps=1,min_samples=7).fit_predict(X)
+#eps=0.3, min_samples=3
+print db
+# db = [np.abs(i+0.1)/np.max(db) for i in db]
+Xsubset = [[X[i][0],X[i][1]] for i in range(len(X)) if db[i]==3]
+x_sub = [x[i] for i in range(len(X)) if db[i]==8]
+for i in x_sub:
+    print i
+
+Xsubset = np.array(Xsubset)
+print Xsubset
+
+plt.figure(2);
+plt.scatter(X[:,0],X[:,1], s=30,c=db, cmap=pylab.cm.jet)
+plt.colorbar()
+
+plt.figure(2);
+plt.scatter(Xsubset[:,0],Xsubset[:,1])
+
+plt.show()
+
+print X
+print Xsubset
